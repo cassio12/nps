aa45599c1726dd2be750d4b3601beb9459452a23
cassioxavier <cassio.fabiano@petronect.com.br>
2020-05-26 13:36:01 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props){
+    super(props);
 
+  }
+
+  hoverEventEnter = (e) => {
+    console.log(e.target)
+    e.target.setAttributte("className", 'select-pointer')
+  }
+
+  hoverEventLeave = (e) => {
+    console.log(e.target)
+    e.target.setAttributte("className", 'diseble-select-pointer')
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="nps-container">
+          <div className="score"><div id="um" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="dois" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>  
+          <div className="score"><div id="tres" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="quatro" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="cinco" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="seis" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="sete" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="oito" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="nove" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>
+          <div className="score"><div id="dez" className="diseble-select-pointer" onMouseEnter={(e) => this.hoverEventEnter(e)} onMouseLeave={(e) => this.hoverEventLeave(e)}></div></div>    
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
